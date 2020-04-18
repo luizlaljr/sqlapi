325f2ad89c9a76aed182b0890eaee44dc91542eb
luizlaljr <luizlaljr@gmail.com>
2020-04-18 15:41:34 -0300

    Fixed bug

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: yarn src/app/server.js
--- src/app/server.js
+++ src/app/server.js
@@ -10,8 +10,9 @@ require('../app');
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(routes);
-app.use(cors);
+
 
 app.listen(3333);
