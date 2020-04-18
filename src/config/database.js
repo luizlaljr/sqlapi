62fcf8daf8eaa6b2deffe2ad8fe35e1586dbee81
luizlaljr <luizlaljr@gmail.com>
2020-04-18 16:46:15 -0300

    Fixed bug to launch the server

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,23 @@ require('dotenv').config({
 });
 
 module.exports = {
+        dialect: 'postgres',
+        url: process.env.DB_URL,
+        use_env_variable: process.env.DB_URL,
+        define: {
+            timestamp: true,
+            underscored: true,
+        },
+};
+
+/* development: {
     dialect: 'postgres',
-    url: process.env.DB_URL,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASS,
+    username: process.env.DB_USER,
+    name: process.env.DB_NAME,
     define: {
         timestamp: true,
         underscored: true,
     },
-};
+}, */
