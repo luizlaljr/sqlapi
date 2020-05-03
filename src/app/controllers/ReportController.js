68829c381af6ec97fcfc7d1899a194497b0cd310
luizlaljr <luizlaljr@gmail.com>
2020-05-03 10:02:41 -0300

    Added order in result

--- src/app/controllers/ReportController.js
+++ src/app/controllers/ReportController.js
@@ -19,7 +19,7 @@ module.exports = {
 
             initDate = base_date_condition(condition,date_condition);
             
-            const users = await User.sequelize.query(`SELECT crews.link, kind, number, locale, start, missions.end, amount, income * posts.factor + transport * 95 as income FROM users LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON users.id = crews.user_id LEFT OUTER JOIN posts ON users.post_id = posts.id WHERE users.id = ${user_id} AND missions.start >= ${initDate}`, {
+            const users = await User.sequelize.query(`SELECT crews.link, kind, number, locale, start, missions.end, amount, income * posts.factor + transport * 95 as income FROM users LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON users.id = crews.user_id LEFT OUTER JOIN posts ON users.post_id = posts.id WHERE users.id = ${user_id} AND missions.start >= ${initDate} ORDER BY start`, {
                 model: User,
                 mapToModel: true,
                 nest: true,
@@ -27,46 +27,6 @@ module.exports = {
             
             res.status(200).json(users);
 
-            /* function base_date_condition() {
-                initdate = new Date();
-                initdate.setDate(1);
-                initdate.setMonth(0);
-                return condition ? date_condition : initdate;
-            }
-
-            const user = await User.findByPk(user_id, {
-                attributes: [],
-                order: [
-                    ['missions', 'start']
-                ],
-                include: [{
-                    association: 'missions',
-                    where: {
-                        start: {
-                            [Op.gte]: base_date_condition()
-                        }
-                    },
-                    attributes: ['income','transport']
-                    ,
-                    through: {
-                        attributes: ['link']
-                    },
-                }, {
-                    association: 'post',
-                    attributes: {
-                        exclude: ['id', 'createdAt', 'updatedAt']
-                    }
-                }]
-            });
-
-
-
-            for (let i = 0; i < user.missions.length; i++) {
-
-                user.missions[i].income = parseFloat((user.missions[i].income * user.post.factor + user.missions[i].transport * 95).toFixed(2));
-            }
-
-            res.status(200).json(user.missions); */
 
         } catch (error) {
             console.log(error)
