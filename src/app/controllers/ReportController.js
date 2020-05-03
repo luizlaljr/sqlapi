b95aa0e0dba3cad1095300ba34df6c00ec047159
luizlaljr <luizlaljr@gmail.com>
2020-05-03 12:46:31 -0300

    Refactored ReportController

--- src/app/controllers/ReportController.js
+++ src/app/controllers/ReportController.js
@@ -19,7 +19,7 @@ module.exports = {
 
             initDate = base_date_condition(condition,date_condition);
             
-            const users = await User.sequelize.query(`SELECT crews.link, kind, number, locale, start, missions.end, amount, income * posts.factor + transport * 95 as income FROM users LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON users.id = crews.user_id LEFT OUTER JOIN posts ON users.post_id = posts.id WHERE users.id = ${user_id} AND missions.start >= ${initDate} ORDER BY start`, {
+            const users = await User.sequelize.query(`SELECT crews.link, kind, number, locale, start, missions.end, amount, income, transport, income * posts.factor + transport * 95 as income FROM users LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON users.id = crews.user_id LEFT OUTER JOIN posts ON users.post_id = posts.id WHERE users.id = ${user_id} AND missions.start >= ${initDate} ORDER BY start`, {
                 model: User,
                 mapToModel: true,
                 nest: true,
