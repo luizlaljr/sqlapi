9d669472c97e5964c2beed78271f5ded32563530
luizlaljr <luizlaljr@gmail.com>
2020-05-19 09:01:04 -0300

    Refactored the ReportController and MissionController

--- src/app/controllers/MissionController.js
+++ src/app/controllers/MissionController.js
@@ -10,7 +10,7 @@ module.exports = {
                     association: 'users',
                     attributes: ['trigram'],
                     through: {
-                        attributes: [],                        
+                        attributes: ['link'],                        
                     },
                 },
             });
--- src/app/controllers/ReportController.js
+++ src/app/controllers/ReportController.js
@@ -2,7 +2,9 @@ const User = require('../models/User');
 const {
     Op
 } = require('sequelize');
-const { base_date_condition } = require('../utils/BaseDateConditions');
+const {
+    base_date_condition
+} = require('../utils/BaseDateConditions');
 
 module.exports = {
 
@@ -17,14 +19,30 @@ module.exports = {
                 date_condition
             } = await User.findByPk(user_id);
 
-            initDate = base_date_condition(condition,date_condition);
-            
-            const users = await User.sequelize.query(`SELECT crews.link, kind, number, locale, start, missions.end, amount, income, transport, income * posts.factor + transport * 95 as income FROM users LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON users.id = crews.user_id LEFT OUTER JOIN posts ON users.post_id = posts.id WHERE users.id = ${user_id} AND missions.start >= ${initDate} ORDER BY start`, {
-                model: User,
-                mapToModel: true,
-                nest: true,
-            });
-            
+            const newDate = new Date();
+            const initDate = `'1/1/${newDate.getFullYear()}'`;
+
+            const dateCondition = new Date(date_condition);
+            const newDateCondition = `'${dateCondition.getMonth()+1}/${dateCondition.getDate()+1}/${dateCondition.getFullYear()}'`;
+
+            let users = new User();
+
+            if (condition) {
+                users = await User.sequelize.query(`SELECT crews.link, kind, number, locale, start, missions.end, amount, income, transport, income * posts.factor + transport * 95 as income FROM users LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON users.id = crews.user_id LEFT OUTER JOIN posts ON users.post_id = posts.id WHERE users.id = ${user_id} AND missions.start >= ${initDate} ORDER BY start`, {
+                    model: User,
+                    mapToModel: true,
+                    nest: true,
+                });
+
+            } else {
+                users = await User.sequelize.query(`SELECT crews.link, kind, number, locale, start, missions.end, amount, income, transport, income * posts.factor + transport * 95 as income FROM users LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON users.id = crews.user_id LEFT OUTER JOIN posts ON users.post_id = posts.id WHERE users.id = ${user_id} AND ((missions.start >= ${initDate} AND crews.link = 'D') OR (missions.start >= ${newDateCondition} AND crews.link = 'G')) ORDER BY start`, {
+                    model: User,
+                    mapToModel: true,
+                    nest: true,
+                });
+            }
+
+
             res.status(200).json(users);
 
 
