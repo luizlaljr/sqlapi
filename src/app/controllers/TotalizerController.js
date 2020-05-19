ab40a62d226df8da3fac369bc9de19d256f68277
luizlaljr <luizlaljr@gmail.com>
2020-05-19 09:20:18 -0300

    "Refactored the TotalizerController and fixed bug"

--- src/app/controllers/TotalizerController.js
+++ src/app/controllers/TotalizerController.js
@@ -1,7 +1,4 @@
 const User = require('../models/User');
-const { base_date_condition } = require('../utils/BaseDateConditions');
-
-const con = require('../index');
 
 module.exports = {
 
@@ -19,13 +16,29 @@ module.exports = {
                 },
             });
 
-            initDate = base_date_condition(user.condition,user.date_condition);
+            const newDate = new Date();
+            const initDate = `'1/1/${newDate.getFullYear()}'`;
+
+            const dateCondition = new Date(date_condition);
+            const newDateCondition = `'${dateCondition.getMonth()+1}/${dateCondition.getDate()+1}/${dateCondition.getFullYear()}'`;
+
+            let users = new User();
+
+            if(condition){
+                users = await User.sequelize.query(`SELECT crews.link, SUM(amount) as amount ,SUM(income) * posts.factor + Sum(transport) * 95 as income FROM users LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON users.id = crews.user_id LEFT OUTER JOIN posts ON users.post_id = posts.id WHERE users.id = ${user_id} AND ((missions.start >= ${initDate} AND crews.link = 'D') OR (missions.start >= ${newDateCondition} AND crews.link = 'C')) GROUP BY crews.link, posts.factor`, {
+                    model: User,
+                    mapToModel: true,
+                    nest: true,
+                });
+
+            }else{
+                users = await User.sequelize.query(`SELECT crews.link, SUM(amount) as amount ,SUM(income) * posts.factor + Sum(transport) * 95 as income FROM users LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON users.id = crews.user_id LEFT OUTER JOIN posts ON users.post_id = posts.id WHERE users.id = ${user_id} AND ((missions.start >= ${initDate} AND crews.link = 'D') OR (missions.start >= ${newDateCondition} AND crews.link = 'G')) GROUP BY crews.link, posts.factor`, {
+                    model: User,
+                    mapToModel: true,
+                    nest: true,
+                });
+            }
             
-            const users = await User.sequelize.query(`SELECT crews.link, SUM(amount) as amount ,SUM(income) * posts.factor + Sum(transport) * 95 as income FROM users LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON users.id = crews.user_id LEFT OUTER JOIN posts ON users.post_id = posts.id WHERE users.id = ${user_id} AND missions.start >= ${initDate} GROUP BY crews.link, posts.factor`, {
-                model: User,
-                mapToModel: true,
-                nest: true,
-            });
             
             const result = {
                 post: user.post.name,
