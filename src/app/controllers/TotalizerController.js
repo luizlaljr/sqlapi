1a4e4add845d8b3743800c4dc3c5c31042e7ae83
luizlaljr <luizlaljr@gmail.com>
2020-11-26 11:00:49 -0300

    Added date_query on totalizer controller

--- src/app/controllers/TotalizerController.js
+++ src/app/controllers/TotalizerController.js
@@ -20,7 +20,7 @@ module.exports = {
                 users = await User.sequelize.query(
                     `SELECT crews.link, SUM(CEILING(amount)) as amount ,SUM(income) * view_base.factor + Sum(transport) * 95 as income FROM view_base 
                     LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON view_base.id = crews.user_id 
-                    WHERE view_base.id = ${user_id} AND missions.start >= view_base.date_condition 
+                    WHERE view_base.id = ${user_id} AND missions.start >= view_base.date_query 
                     GROUP BY crews.link, view_base.factor`, {
                         model: User,
                         mapToModel: true,
@@ -31,7 +31,7 @@ module.exports = {
                 users = await User.sequelize.query(
                     `SELECT crews.link, SUM(amount) as amount ,SUM(income) * view_base.factor + Sum(transport) * 95 as income FROM view_base 
                     LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON view_base.id = crews.user_id 
-                    WHERE view_base.id = ${user_id} AND missions.start >= view_base.date_condition 
+                    WHERE view_base.id = ${user_id} AND missions.start >= view_base.date_query 
                     GROUP BY crews.link, view_base.factor`, {
                         model: User,
                         mapToModel: true,
