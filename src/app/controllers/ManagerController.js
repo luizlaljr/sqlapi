2788c4d3ce4d4dd1e2e04cf718d071382a54cc4b
luizlaljr <luizlaljr@gmail.com>
2021-04-15 16:13:20 -0300

    Bug fixed on manager controller

--- src/app/controllers/ManagerController.js
+++ src/app/controllers/ManagerController.js
@@ -4,17 +4,17 @@ module.exports = {
     async index (req, res) {
         try {
 
-            const users = await User.sequelize.query(`SELECT view_base.id, view_base.antique, view_base.post, view_base.name, view_base.condition, view_base.document, view_base.date_condition, view_base.modulus, view_base.prevision, view_base.operationality, view_base.activity, view_base.project, SUM(CEILING(amount)) as amount ,SUM(income) * view_base.factor + Sum(transport) * 95 as income 
+            const users = await User.sequelize.query(`SELECT view_base.id, view_base.antique, view_base.post, view_base.name, view_base.condition, view_base.date_condition, view_base.document, view_base.modulus, view_base.prevision, view_base.operationality, view_base.activity, view_base.project, SUM(CEILING(amount)) as amount ,SUM(income) * view_base.factor + Sum(transport) * 95 as income 
             FROM view_base 
             LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON view_base.id = crews.user_id  
             WHERE missions.start >= view_base.date_condition AND view_base.condition = true AND crews.link = 'C'
-            GROUP BY view_base.id, view_base.post, view_base.name, view_base.antique, view_base.condition, view_base.document, view_base.date_condition, view_base.modulus, view_base.prevision, view_base.operationality, view_base.activity, view_base.project, view_base.factor
+            GROUP BY view_base.id, view_base.antique, view_base.post, view_base.name, view_base.condition, view_base.date_condition, view_base.document, view_base.modulus, view_base.prevision, view_base.operationality, view_base.activity, view_base.project, view_base.factor
             UNION
-            SELECT view_base.id, view_base.antique, view_base.post, view_base.name, view_base.condition,  view_base.date_condition, view_base.document, view_base.modulus, view_base.prevision, view_base.operationality, view_base.activity, view_base.project, SUM(amount) as amount ,SUM(income) * view_base.factor + Sum(transport) * 95 as income 
+            SELECT view_base.id, view_base.antique, view_base.post, view_base.name, view_base.condition, view_base.date_condition, view_base.document, view_base.modulus, view_base.prevision, view_base.operationality, view_base.activity, view_base.project, SUM(amount) as amount ,SUM(income) * view_base.factor + Sum(transport) * 95 as income 
             FROM view_base 
             LEFT OUTER JOIN ( crews INNER JOIN missions ON missions.id = crews.mission_id) ON view_base.id = crews.user_id 
             WHERE missions.start >= view_base.date_condition AND view_base.condition = false AND crews.link = 'D'
-            GROUP BY view_base.id, view_base.post, view_base.name, view_base.antique, view_base.condition, view_base.document, view_base.date_condition, view_base.modulus, view_base.prevision, view_base.operationality, view_base.activity, view_base.project, view_base.factor
+            GROUP BY view_base.id, view_base.antique, view_base.post, view_base.name, view_base.condition, view_base.date_condition, view_base.document, view_base.modulus, view_base.prevision, view_base.operationality, view_base.activity, view_base.project, view_base.factor
             ORDER BY antique`, {
                 model: User,
                 mapToModel: true,
@@ -25,6 +25,7 @@ module.exports = {
         } catch (error) {
             return res.status(500).json({
                 "message-error": "There was a problem when handling this request to list users.",
+                "error": error
             });
         }
     }
